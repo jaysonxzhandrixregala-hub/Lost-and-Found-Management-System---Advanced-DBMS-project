73a83882ea01e6461a9a15fbbcd1b9ba05c2ad60
Jayson Xzhandrix Regala <jumong@Jumongs-Macbook-Air.local>
2026-08-13 12:50:39 +0800

    Made designs on forms. Frontended by Charmine Ner

---
+++ LostAndFound-LoginRegister/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class loginForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(loginForm))
+        Label1 = New Label()
+        usrBox = New TextBox()
+        loginbtn = New Button()
+        Label2 = New Label()
+        registryLink = New LinkLabel()
+        Panel1 = New Panel()
+        passwordBox = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI Semibold", 75F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.White
+        Label1.LiveSetting = Automation.AutomationLiveSetting.Assertive
+        Label1.Location = New Point(1270, 86)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(333, 133)
+        Label1.TabIndex = 0
+        Label1.Text = "Log In"
+        ' 
+        ' usrBox
+        ' 
+        usrBox.BackColor = Color.SteelBlue
+        usrBox.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        usrBox.ForeColor = SystemColors.ScrollBar
+        usrBox.Location = New Point(1284, 287)
+        usrBox.Name = "usrBox"
+        usrBox.Size = New Size(293, 33)
+        usrBox.TabIndex = 1
+        usrBox.Text = "Username"
+        ' 
+        ' loginbtn
+        ' 
+        loginbtn.BackColor = Color.DodgerBlue
+        loginbtn.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        loginbtn.ForeColor = Color.White
+        loginbtn.Location = New Point(1361, 454)
+        loginbtn.Name = "loginbtn"
+        loginbtn.Size = New Size(137, 33)
+        loginbtn.TabIndex = 2
+        loginbtn.Text = "Log In"
+        loginbtn.UseVisualStyleBackColor = False
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = Color.White
+        Label2.Location = New Point(1284, 548)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(171, 21)
+        Label2.TabIndex = 3
+        Label2.Text = "Don't have an account?"
+        ' 
+        ' registryLink
+        ' 
+        registryLink.AutoSize = True
+        registryLink.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        registryLink.ForeColor = Color.FromArgb(CByte(59), CByte(54), CByte(75))
+        registryLink.LinkColor = Color.DodgerBlue
+        registryLink.Location = New Point(1451, 548)
+        registryLink.Name = "registryLink"
+        registryLink.Size = New Size(58, 21)
+        registryLink.TabIndex = 4
+        registryLink.TabStop = True
+        registryLink.Text = "Sign In"
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.DodgerBlue
+        Panel1.BackgroundImage = CType(resources.GetObject("Panel1.BackgroundImage"), Image)
+        Panel1.BackgroundImageLayout = ImageLayout.Stretch
+        Panel1.Location = New Point(-1, -3)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(961, 1058)
+        Panel1.TabIndex = 5
+        ' 
+        ' passwordBox
+        ' 
+        passwordBox.BackColor = Color.SteelBlue
+        passwordBox.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        passwordBox.ForeColor = Color.DarkGray
+        passwordBox.Location = New Point(1284, 363)
+        passwordBox.Name = "passwordBox"
+        passwordBox.Size = New Size(293, 33)
+        passwordBox.TabIndex = 6
+        passwordBox.Text = "Password"
+        ' 
+        ' loginForm
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(0), CByte(74), CByte(150))
+        CausesValidation = False
+        ClientSize = New Size(1904, 1041)
+        Controls.Add(passwordBox)
+        Controls.Add(Panel1)
+        Controls.Add(registryLink)
+        Controls.Add(Label2)
+        Controls.Add(loginbtn)
+        Controls.Add(usrBox)
+        Controls.Add(Label1)
+        Name = "loginForm"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents usrBox As TextBox
+    Friend WithEvents loginbtn As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents registryLink As LinkLabel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents passwordBox As TextBox
+
+End Class
---
+++ LostAndFound-LoginRegister/Form2.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class registryForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        registry_lbl = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        username_textbox = New TextBox()
+        email_textbox = New TextBox()
+        password_textbox = New TextBox()
+        register_button = New Button()
+        Label1 = New Label()
+        loginlink = New LinkLabel()
+        Panel1 = New Panel()
+        Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' registry_lbl
+        ' 
+        registry_lbl.AutoSize = True
+        registry_lbl.Font = New Font("Segoe UI Semibold", 72F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        registry_lbl.ForeColor = Color.White
+        registry_lbl.Location = New Point(24, 21)
+        registry_lbl.Margin = New Padding(2, 0, 2, 0)
+        registry_lbl.Name = "registry_lbl"
+        registry_lbl.Size = New Size(826, 128)
+        registry_lbl.TabIndex = 0
+        registry_lbl.Text = "Registration Form"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = Color.SteelBlue
+        TextBox1.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox1.ForeColor = SystemColors.ScrollBar
+        TextBox1.Location = New Point(193, 221)
+        TextBox1.Margin = New Padding(2, 1, 2, 1)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(258, 29)
+        TextBox1.TabIndex = 1
+        TextBox1.Text = "First Name"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = Color.SteelBlue
+        TextBox2.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox2.ForeColor = SystemColors.ScrollBar
+        TextBox2.Location = New Point(471, 221)
+        TextBox2.Margin = New Padding(2, 1, 2, 1)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(258, 29)
+        TextBox2.TabIndex = 2
+        TextBox2.Text = "Last Name"
+        ' 
+        ' username_textbox
+        ' 
+        username_textbox.BackColor = Color.SteelBlue
+        username_textbox.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        username_textbox.ForeColor = SystemColors.ScrollBar
+        username_textbox.Location = New Point(193, 309)
+        username_textbox.Margin = New Padding(2, 1, 2, 1)
+        username_textbox.Name = "username_textbox"
+        username_textbox.Size = New Size(536, 29)
+        username_textbox.TabIndex = 3
+        username_textbox.Text = "Username"
+        ' 
+        ' email_textbox
+        ' 
+        email_textbox.BackColor = Color.SteelBlue
+        email_textbox.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        email_textbox.ForeColor = SystemColors.ScrollBar
+        email_textbox.Location = New Point(193, 388)
+        email_textbox.Margin = New Padding(2, 1, 2, 1)
+        email_textbox.Name = "email_textbox"
+        email_textbox.Size = New Size(536, 29)
+        email_textbox.TabIndex = 5
+        email_textbox.Text = "Email"
+        ' 
+        ' password_textbox
+        ' 
+        password_textbox.BackColor = Color.SteelBlue
+        password_textbox.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        password_textbox.ForeColor = SystemColors.ScrollBar
+        password_textbox.Location = New Point(193, 476)
+        password_textbox.Margin = New Padding(2, 1, 2, 1)
+        password_textbox.Name = "password_textbox"
+        password_textbox.Size = New Size(536, 29)
+        password_textbox.TabIndex = 6
+        password_textbox.Text = "Password"
+        ' 
+        ' register_button
+        ' 
+        register_button.BackColor = Color.DodgerBlue
+        register_button.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        register_button.Location = New Point(579, 567)
+        register_button.Margin = New Padding(2, 1, 2, 1)
+        register_button.Name = "register_button"
+        register_button.Size = New Size(150, 34)
+        register_button.TabIndex = 7
+        register_button.Text = "Register"
+        register_button.UseVisualStyleBackColor = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.White
+        Label1.Location = New Point(203, 701)
+        Label1.Margin = New Padding(2, 0, 2, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(186, 21)
+        Label1.TabIndex = 8
+        Label1.Text = "Already have an account?"
+        ' 
+        ' loginlink
+        ' 
+        loginlink.AutoSize = True
+        loginlink.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        loginlink.ForeColor = Color.White
+        loginlink.LinkColor = Color.DodgerBlue
+        loginlink.Location = New Point(383, 701)
+        loginlink.Margin = New Padding(2, 0, 2, 0)
+        loginlink.Name = "loginlink"
+        loginlink.Size = New Size(53, 21)
+        loginlink.TabIndex = 9
+        loginlink.TabStop = True
+        loginlink.Text = "Log In"
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.FromArgb(CByte(0), CByte(74), CByte(150))
+        Panel1.Controls.Add(registry_lbl)
+        Panel1.Controls.Add(loginlink)
+        Panel1.Controls.Add(TextBox1)
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(TextBox2)
+        Panel1.Controls.Add(register_button)
+        Panel1.Controls.Add(username_textbox)
+        Panel1.Controls.Add(password_textbox)
+        Panel1.Controls.Add(email_textbox)
+        Panel1.ForeColor = Color.Transparent
+        Panel1.Location = New Point(564, 101)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(866, 827)
+        Panel1.TabIndex = 10
+        ' 
+        ' registryForm
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.LightGray
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(1904, 1041)
+        Controls.Add(Panel1)
+        DoubleBuffered = True
+        Margin = New Padding(2, 1, 2, 1)
+        Name = "registryForm"
+        Text = "Form2"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents registry_lbl As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents username_textbox As TextBox
+    Friend WithEvents email_textbox As TextBox
+    Friend WithEvents password_textbox As TextBox
+    Friend WithEvents register_button As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents loginlink As LinkLabel
+    Friend WithEvents Panel1 As Panel
+End Class
