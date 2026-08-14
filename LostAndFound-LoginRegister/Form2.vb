d1a9278a1e9d95ef5a71e5e3dd07d8d601ec25f4
jaysonxzhandrixregala-hub <jaysonxzhnadrix.regala@normi.edu.ph>
2026-08-15 03:18:27 +0800

    Changed the positions of the elements inside the login form to balance.

---
+++ LostAndFound-LoginRegister/Form2.vb
@@ -0,0 +1,18 @@
+﻿Public Class registryForm
+    Private Sub register_button_Click(sender As Object, e As EventArgs) Handles register_button.Click
+
+    End Sub
+
+    Private Sub loginlink_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles loginlink.LinkClicked
+        loginForm.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub registryForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
